a53a7a8d2d30b76585fdddb7557636e1e2032746
ZorsCode <zhosyuy21@gmail.com>
2023-08-04 18:12:41 +0800

    modified page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
         <Weather />
       </header>
     </div>
